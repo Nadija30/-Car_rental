a12390e45fa1e74c1b27ecfa23a5a43631242bdd
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2024-02-04 14:49:33 +0200

    commit

--- src/components/CarsList/FavoritesList.jsx
+++ src/components/CarsList/FavoritesList.jsx
@@ -6,21 +6,24 @@ import { filterCars } from '../../shared/utils';
 
 import CarItem from 'components/CarItem/CarItem';
 import NotFound from 'components/NotFound';
+import listCss from './CarsList.module.css';
+import pagesCss from '../../pages/Pages.module.css';
 
 const FavoritesList = () => {
   const favoriteCars = useSelector(selectFavorites);
   const filter = useSelector(selectFilter);
   const filteredCars = filterCars(favoriteCars, filter);
-
   return (
-    <div className="flex flex-col items-center justify-center gap-5 pb-4">
-      {!filteredCars.length && <NotFound />}
-      <ul className="cards-list">
-        {filteredCars.map(car => (
-          <CarItem car={car} key={car.id} />
-        ))}
-      </ul>
-    </div>
+    <section className={pagesCss.section}>
+      <div className={pagesCss.container}>
+        {!filteredCars.length && <NotFound />}
+        <ul className={listCss.carsList}>
+          {filteredCars.map(car => (
+            <CarItem car={car} key={car.id} />
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -4,11 +4,7 @@ const FavoritePage = () => {
   return (
     <section className={pagesCss.section}>
       <div className={pagesCss.container} style={{ '--top': '50px' }}>
-        <div className="mt-[-3rem] w-full wide:mt-[-8rem]">
-          <div className="relative mx-auto my-0 w-full ">
-            <Sidebar />
-          </div>
-        </div>
+        <Sidebar />
       </div>
     </section>
   );
